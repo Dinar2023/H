da9903c3ab78ecd8d3366f77c97ce53adb88bfcb
Dinar <dinar-bogdanov@yandex.ru>
2023-11-14 23:20:47 +0500

    задача с If

---
+++ Любимчик/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите свое имя");
+string UserName = Console.ReadLine();
+if (UserName.ToLower()== "alice") //ToLower()-переводит в нижний регистр
+
+{
+    Console.WriteLine("Привет Алисонька^^");
+}
+else
+{
+Console.WriteLine("Привет " + UserName);  
+}   
