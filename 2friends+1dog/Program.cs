83aba59f6f1f7b6121254014d4256f565bb5c1dd
Dinar <dinar-bogdanov@yandex.ru>
2023-11-18 10:21:16 +0500

    2friend+1dog

---
+++ 2friends+1dog/Program.cs
@@ -0,0 +1,24 @@
+﻿int FriendSpeed1 = 9;
+int FriendSpeed2 = 7;
+int DogSpeed = 200;
+float distance = 200000;
+float time = 0;
+bool F1 = true;
+int count = 0;
+while (distance > 20)
+ {
+    if (F1==true)
+    {time=distance/(FriendSpeed2+DogSpeed);
+    distance=distance-time*(FriendSpeed1+FriendSpeed2);
+    count++;
+    }
+    else
+    {
+    time=distance/(FriendSpeed1+DogSpeed);
+    distance=distance-time*(FriendSpeed1+FriendSpeed2);
+    count++;  
+    }
+ }
+
+
+Console.WriteLine(count);
