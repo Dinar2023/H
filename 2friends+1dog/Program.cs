486e76f90c8e50a84e30b7151b5421255a269c19
Dinar <dinar-bogdanov@yandex.ru>
2023-11-18 10:57:33 +0500

    нашел ошибку с логической переменной, + добавил ввод данных в ручную

--- 2friends+1dog/Program.cs
+++ 2friends+1dog/Program.cs
@@ -1,24 +1,35 @@
-﻿int FriendSpeed1 = 9;
-int FriendSpeed2 = 7;
-int DogSpeed = 200;
-float distance = 200000;
-float time = 0;
+﻿Console.WriteLine("Введите скорость первого друга");
+string fr1 = Console.ReadLine();
+//int Fr1Speed = Convert.ToInt16(fr1);
+int FriendSpeed1 = Convert.ToInt16(fr1);
+Console.WriteLine("Введите скорость второго друга");
+fr1 = Console.ReadLine();
+int FriendSpeed2 = Convert.ToInt16(fr1);
+Console.WriteLine("Введите скорость второго собаки");
+fr1 = Console.ReadLine();
+int DogSpeed = Convert.ToInt16(fr1);
+Console.WriteLine("Введите дистанцию между друзьями");
+fr1 = Console.ReadLine();
+Double distance = Convert.ToDouble(fr1);
+Double time = 0;
 bool F1 = true;
 int count = 0;
 while (distance > 20)
  {
     if (F1==true)
     {time=distance/(FriendSpeed2+DogSpeed);
-    distance=distance-time*(FriendSpeed1+FriendSpeed2);
-    count++;
+    //Console.WriteLine("F1 "+distance + " " + time);
+    F1=false;
     }
     else
     {
     time=distance/(FriendSpeed1+DogSpeed);
-    distance=distance-time*(FriendSpeed1+FriendSpeed2);
-    count++;  
+    //Console.WriteLine("F2 "+distance + " " + time);
+    F1=true;
     }
+    distance=distance-time*(FriendSpeed1+FriendSpeed2);
+    count++;
  }
 
 
-Console.WriteLine(count);
+Console.WriteLine("Собака сделает " + count + " разворотов");
